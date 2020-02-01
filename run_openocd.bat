d16c6b59b496c127054c37a498879e95152d74d7
Alexey Nikolaev <alexeynikzzz@gmail.com>
2020-02-01 15:08:26 +0300

    -Added config files for OpenOCD and let them run as default

--- run_openocd.bat
+++ run_openocd.bat
@@ -1,6 +1,6 @@
-@rem connect to debugger command line by telnet
+@echo off
+rem connect to debugger command line by telnet
 start telnet localhost 4444
 
-start /min openocd  -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\interface\stlink.cfg"^
-         -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\target\stm32f1x.cfg"^
-         -s "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts"
+start /min openocd  
+
