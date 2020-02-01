4d1ab91f3348a16314ba294483a56f2e31a45910
Alexey Nikolaev <alexeynikzzz@gmail.com>
2020-02-01 14:02:23 +0300

    -Made OpenOCD run server and opened separate window for telnet client

--- run_openocd.bat
+++ run_openocd.bat
@@ -1,1 +1,6 @@
-openocd -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\interface\stlink.cfg" -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\target\stm32f1x.cfg" -s "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts"
+@rem connect to debugger command line by telnet
+start telnet localhost 4444
+
+start /min openocd  -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\interface\stlink.cfg"^
+         -f "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts\target\stm32f1x.cfg"^
+         -s "C:\Program Files (x86)\OpenOCD-20191029-0.10.0\share\openocd\scripts"
